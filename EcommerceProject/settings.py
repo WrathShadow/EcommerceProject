9afbb5947de16cfde69dcc1afd4ff03db6a390d0
Wrath Shadow <ananthukrishnanr22597@gmail.com>
2022-07-20 19:23:45 +0530

    settings database config updated

--- EcommerceProject/settings.py
+++ EcommerceProject/settings.py
@@ -80,11 +80,8 @@ WSGI_APPLICATION = 'EcommerceProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shop',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
